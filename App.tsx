924c7ec5abbbce7a81d0bcba620e8374d3c09380
L3viPlayzz1014 <levidebest661@hotmail.com>
2025-12-12 11:18:10 +0100

    Fix import path for queryClient

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
+import { queryClient } from "./queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -33,3 +33,4 @@ function App() {
 }
 
 export default App;
+
